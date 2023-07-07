1931ab1e54474c608585c30b464ef690aa19de14
Kirill Kudr <94325208+dulyas@users.noreply.github.com>
2023-07-08 04:14:16 +0500

    fix basepath

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -12,5 +13,6 @@ export default defineConfig({
       ],
     }
   },
+  base: '',
   optimizeDeps: {exclude: ["axios"]},
 })
